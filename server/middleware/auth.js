78e4e0d8c61cadd2cb5fbbf3e4ce29b72e4023c4
Navneet Pratap <pratap102718@gmail.com>
2026-05-19 23:13:43 +0530

    update secret retrieval logic in auth middleware

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken'
 import dotenv from 'dotenv'
 
 dotenv.config()
-const SECRET = process.env.JWT_SECRET
+const SECRET = process.env.SECRET || process.env.JWT_SECRET
 
 const auth = async (req, res, next) => {
     try {
